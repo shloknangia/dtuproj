5eda10c68179e10346c536fde1c381fd4c25038a
Rohan <rohan.vbh@gmail.com>
2016-10-06 21:06:33 +0530

    just before linking the specializations to the user_id

--- InfoPage/models.py
+++ InfoPage/models.py
@@ -1,10 +1,18 @@
+import datetime
 from django.db import models
+
+from django.utils import timezone
 
 class student_name(models.Model):
 	student_name = models.CharField(max_length=50)
 	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.student_name
+	def was_updated_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Specialisation(models.Model):
 	spec_text = models.CharField(max_length=500)
-
+	def __str__(self):
+		return self.spec_text
 
