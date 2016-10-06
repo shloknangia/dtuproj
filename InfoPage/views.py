1b161ad9be8238828cc7395718f02796de953a63
Rohan <rohan.vbh@gmail.com>
2016-10-06 19:51:26 +0530

    main page links to student pages

--- InfoPage/views.py
+++ InfoPage/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
+
+from django.template import loader
+
 from .models import student_name
 
 def index(request):
     latest_student_added = student_name.objects.order_by('-pub_date')[:5]
-    output = ','.join([s.student_name for s in latest_student_added])
-    return HttpResponse(output)
+    
+    template = loader.get_template('InfoPage/index.html')
+    context = {
+    'latest_student_added':
+    latest_student_added,
+    }
+    return HttpResponse(template.render(context,request))
+
     
 def user_detail(request, user_id):
 	response = "This is the page for student # - %s. <br> We can also display the names of students corresponding to the user_id."
