2df1d347f54fc069dc34bb8f4f86a167ab43220d
Rohan <rohan.vbh@gmail.com>
2016-10-07 15:27:51 +0530

    when we didnt get selected to do the project

--- InfoPage/admin.py
+++ InfoPage/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Specialisation,student_name
+from .models import student_name
 
 admin.site.register(student_name)
-admin.site.register(Specialisation)
+#admin.site.register(Specialisation)
 # Register your models here.
--- InfoPage/models.py
+++ InfoPage/models.py
@@ -6,14 +6,15 @@ from django.utils import timezone
 class student_name(models.Model):
 	student_name = models.CharField(max_length=50)
 	pub_date = models.DateTimeField('date published')
+	spec1 = models.TextField(max_length=500, default="Enter specialization information here.")
 	def __str__(self):
 		return self.student_name
 	def was_updated_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-class Specialisation(models.Model):
-	student=models.ForeignKey(student_name, on_delete=models.CASCADE, default=0)
-	spec_text = models.CharField(max_length=500)
-	def __str__(self):
-		return self.spec_text
+#class Specialisation(models.Model):
+#	student=models.ForeignKey(student_name, on_delete=models.CASCADE, default=0)
+#	spec_text = models.CharField(max_length=500)
+#	def __str__(self):
+#		return self.spec_text
 
